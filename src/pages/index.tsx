410c1b48e023050743ff48db512088bee4e7740b
AHZU DIRECT TECH LLC <ahzudirect@gmail.com>
2023-04-16 09:08:05 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,7 @@ import Section1 from '../components/home/section-1'
 export default function Home() {
   return (
     <div className="bg-gray-100">
-      <Head>
-        <title>Ahzu Direct Tech LLC</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Section1 />
-      
     </div>
   )
 }
